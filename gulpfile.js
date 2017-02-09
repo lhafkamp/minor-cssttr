7ba9c15ac5f7b9e55465cfc2d633b068b011f041
lhafkamp <Luuk@Luuks-MacBook-Pro.local>
2017-02-09 17:10:48 +0100

    login + nav components

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,15 @@ var gulp        = require('gulp');
 var watch       = require('gulp-watch');
 var browserSync = require('browser-sync').create();
 
-gulp.task('default', ['watch:css', 'browser-sync']);
+gulp.task('default', ['watch:css', 'watch:html', 'browser-sync']);
+
+gulp.task('watch:html', function() {
+	return gulp.watch('styleguide.html', ['sync:html']);
+});
+
+gulp.task('sync:html', function() {
+	gulp.src('styleguide.html').pipe(browserSync.stream());
+})
 
 gulp.task('watch:css', function() {
 	return gulp.watch('styles.css', ['sync:css']);
