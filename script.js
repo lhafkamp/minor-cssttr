382fa7a926e48e93a303102a2d4fec1c10b3edc9
lhafkamp <Luuk@Luuks-MacBook-Pro.local>
2017-02-12 14:59:15 +0100

    some minor adjustments

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const button = document.querySelector('button');
 const dialog = document.querySelector('.popup');
 const main = document.querySelector('main');
+const progress = document.querySelector('progress');
+const proNav = Array.from(document.querySelectorAll('nav ol li a'));
 
 function openDialog() {
 	dialog.style.display = 'block';
@@ -12,5 +14,17 @@ function removeDialog() {
 	main.classList.remove('de-emphasized');
 }
 
+// TODO add a's
+function addProgress() {
+	if (progress.value < 0.26) {
+		proNav[0].style.color = '#2ecc71';
+	} else {
+		proNav[1].style.color = '#2ecc71';
+	}
+}
+
+addProgress();
+
 button.addEventListener('click', openDialog);
 dialog.addEventListener('click', removeDialog);
+progress.addEventListener('click', addProgress);
