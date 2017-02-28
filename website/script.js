d915be36cb8cf94a2cfabcc76cf25527a0e671ac
lhafkamp <Luuk@193.141.028.145.hva.nl>
2017-02-28 15:31:56 +0100

    loading fixed

--- website/script.js
+++ website/script.js
@@ -1,27 +1,9 @@
 const button = document.querySelector('.login button');
-const dialog = document.querySelector('.popup');
 const main = document.querySelector('main');
-const proNav = Array.from(document.querySelectorAll('nav ol li a'));
 const form = Array.from(document.querySelectorAll('form input'));
 const loaderbox = document.querySelector('#loaderbox');
-
-// the definition of hacky
-setTimeout(function() {
-	loaderbox.style.opacity = 0;
-	setTimeout(function() {
-		loaderbox.style.display = 'none';
-	}, 850);
-}, 800);
-
-function openDialog() {
-	dialog.style.display = 'block';
-	main.classList.add('de-emphasized');
-}
-
-function removeDialog() {
-	dialog.style.display = 'none';
-	main.classList.remove('de-emphasized');
-}
+const intro = document.querySelector('.intro');
+const login = document.querySelector('.login');
 
 function enableButton() {
 	if (form[0].value !== '' && form[1].value !== '') {
@@ -30,25 +12,21 @@ function enableButton() {
 	}
 }
 
-button.addEventListener('click', openDialog);
-dialog.addEventListener('click', removeDialog);
-form[1].addEventListener('input', enableButton);
-
-
-// nav focus
-
-const product = document.querySelector('.product');
-const subnav = document.querySelector('.menu li ul');
-
-
-function showNav(e) {
-	if (e.code === 'Tab' && e.target.classList.contains('product') || e.target.classList.contains('hi')) {
-		subnav.style.padding = '2em 1em 1em 0';
-		subnav.style.position = 'absolute';
-		subnav.style.display = 'block';
-	} else {
-		subnav.style.display = 'none';
-	}
+// the definition of hacky
+function nextPage() {
+	loaderbox.style.opacity = 1;
+	loaderbox.style.display = 'block';
+	setTimeout(function() {
+		loaderbox.style.opacity = 0;
+		setTimeout(function() {
+			loaderbox.style.display = 'none';
+		}, 850);
+		intro.classList.remove('hide');
+		intro.style.display = 'flex';
+		login.classList.add('hide');
+	}, 800);
 }
 
-document.addEventListener('keyup', showNav);
+button.addEventListener('click', nextPage);
+form[1].addEventListener('input', enableButton);
+
